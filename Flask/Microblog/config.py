7e40957c7b6f4ac08b23077cc958fd316b366055
Clinton Oxendine <clintoxen@gmail.com>
2018-06-29 15:22:38 -0400

    heroku deployment changes

--- Flask/Microblog/config.py
+++ Flask/Microblog/config.py
@@ -20,4 +20,5 @@ class Config(object):
     POSTS_PER_PAGE = 10
     LANGUAGES = ['en', 'es']
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
