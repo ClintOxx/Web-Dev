c6b295950cda11a1d3c89fa1dfe8960b22fc2e4b
Clinton Oxendine <clintoxen@gmail.com>
2018-07-04 23:06:51 -0400

    update

--- Flask/Flask Restful API/Full Rest Api/app.py
+++ Flask/Flask Restful API/Full Rest Api/app.py
@@ -1,8 +1,4 @@
-from flask import Flask, jsonify, request,  render_template
-
-app = Flask(__name__)
-
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
@@ -13,19 +9,27 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name: 
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None) #going to loop through list, "items" is passed into x arg
+        return {'item': item}, 200 if item else 404
     
     def post(self, name):
-        item = {'name' : name, 'price': 12.00}
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {"message": f"item with name {name} already exists"}, 400
+        
+        data = request.get_json()
+        item = {'name' : name, 'price': data['price']}
         items.append(item)
         return item, 201
 
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
 #let api know the Item resource is accessible by api, passing the name to the Item class like @app.route
 api.add_resource(Item, '/item/<string:name>') 
-
+api.add_resource(ItemList, '/items')
 
 
 app.run(debug=True)
