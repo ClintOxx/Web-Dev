9b73b23c592a46524f5fc52db973f3ac17cad73c
Clinton Oxendine <clintoxen@gmail.com>
2018-07-05 23:52:07 -0400

    added database and inserting records

--- Flask/Flask Restful API/Full Rest Api/app.py
+++ Flask/Flask Restful API/Full Rest Api/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse
-from flask_jwt import JWT, jwt_required
-
-
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 from security import authenticate, identity
 
 
+from item import Item, ItemList
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = 'github' 
@@ -13,58 +13,15 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity) #/auth
 
-items = []
-
-class Item(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('price', type=float, required=True, help='This field cant be blank')
-
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None) #going to loop through list, "items" is passed into x arg
-        return {'item': item}, 200 if item else 404
-    
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {"message": f"item with name {name} already exists"}, 400
-        
-        data = Item.parser.parse_args()
-        item = {'name' : name, 'price': data['price']}
-        items.append(item)
-        return item, 201
-
-
-    def delete(self, name):
-        global items
-        items = list(filter(lambda x:x['name'] !=name, items))
-        return {'message': 'ITem deleted'}
-
-    def put(self, name):
-
-        data = Item.parser.parse_args()
-
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        if item is None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
-        else:
-            item.update(data)
-        return item
-
-
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
-
 
 #let api know the Item resource is accessible by api, passing the name to the Item class like @app.route
 api.add_resource(Item, '/item/<string:name>') 
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register') #call UserRegister post method
 
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
--- Flask/Flask Restful API/Full Rest Api/security.py
+++ Flask/Flask Restful API/Full Rest Api/security.py
@@ -1,22 +1,14 @@
 from user import User
 
-users = [User(1, 'bob', 'asdf' )]
-
-
-username_mapping = { u.username: u for u in users} #looping through users list, we make the username(bob) the key. 
-#The values of the key are the objects "bob" class properties
-
-
-userid_mapping = { u.id: u for u in users} # the key:value pair in here is the id is the key, bobs propties are the value
 
 
 def authenticate(username, password):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and user.password == password:
         return user
 
 
 def identity(payload):
     user_id = payload['identity']
-    return userid_mapping.get(user_id, None)
+    return User.find_by_id(user_id)
 
