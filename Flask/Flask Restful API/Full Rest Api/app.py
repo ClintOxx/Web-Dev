8c29aba982ad936bd0c87d546a32e42714426e37
Clinton Oxendine <clintoxen@gmail.com>
2018-07-09 11:08:51 -0400

    sort

--- Flask/Flask Restful API/Full Rest Api/app.py
+++ Flask/Flask Restful API/Full Rest Api/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-from flask_restful import Api
 from flask_jwt import JWT
-from security import authenticate, identity
+from flask_restful import Api
 
-from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from resources.user import UserRegister
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///data.db'
@@ -32,10 +32,3 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app) # avoid circular imports
     app.run(debug=True)
-
-
-
-
-
-
-
